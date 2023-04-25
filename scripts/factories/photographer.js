5685e71c5c55b2e35d97d4ec291c01901bf7054b
Dupont <zeshin59@gmail.com>
2023-04-25 19:34:03 +0200

    index fini sauf lien pour photographer page

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,18 +1,38 @@
 function photographerFactory(data) {
-    const { name, portrait } = data;
+    const { name, city, country, tagline, price, portrait} = data;
 
     const picture = `assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
-        const divImg = document.createElement('div');
+        const link = document.createElement('a');
         const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
         const h2 = document.createElement( 'h2' );
+        const localisation = document.createElement("span");
+        const spanTagline = document.createElement('span');
+        const spanPrice = document.createElement("span");
+
+        img.setAttribute("src", picture);
+        img.setAttribute("alt", name);
         h2.textContent = name;
-        divImg.appendChild(img);
-        article.appendChild(divImg);
-        article.appendChild(h2);
+        link.href = "#";
+        localisation.textContent = `${city}, ${country}`;
+        spanTagline.textContent = tagline;
+        spanPrice.textContent = `${price}€/jour`;
+
+        img.classList.add("photographer_portrait")
+        h2.classList.add("photographer_name");
+        localisation.classList.add("photographer_localisation");
+        spanTagline.classList.add("photographer_tagline");
+        spanPrice.classList.add("photographer_price");
+
+        link.appendChild(img);
+        link.appendChild(h2);
+        article.appendChild(link);
+        article.appendChild(localisation);
+        article.appendChild(spanTagline);
+        article.appendChild(spanPrice);
+        
         return (article);
     }
     return { name, picture, getUserCardDOM }
